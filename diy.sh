ce1b2715afda2401fd1236ad94c1f8e611ae182e
buxiaomo <95112082@qq.com>
2019-12-13 01:57:17 +0800

    优化内核参数, root默认密码: root

--- diy.sh
+++ diy.sh
@@ -11,3 +11,6 @@ sed -i "s/3.openwrt.pool.ntp.org/ntp4.aliyun.com/g" ./package/base-files/files/b
 echo "Settinng Timezone"
 sed -i "s/UTC/CST-8/g" ./package/base-files/files/bin/config_generate
 sed -i "/CST-8/a set system.@system[-1].zonename='Asia/Shanghai'" ./package/base-files/files/bin/config_generate
+
+# set root password(default: root)
+sed -i 's|root.*|root:$1$n3vjdweX$vyZqcZyUC5Q2uq4bxnfbQ0:18242:0:99999:7:::|g' ./package/base-files/files/etc/shadow
